eb30809adf21637fa113f645292526920ce8298d
alexandremathieu <alexandre.mathieu.7@ulaval.ca>
2023-01-10 13:59:34 -0500

    feat: update meta infos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Presentation from "../components/presentation/Presentation";
 import Experiences from "../components/experiences/Experiences";
 import CodeExamples from "../components/codeexamples/CodeExamples";
@@ -23,12 +24,35 @@ export default function Home({
     experiences: Experience[];
 }) {
     return (
-        <div className="bg-lightMode dark:bg-darkMode overflow-x-hidden overflow-y-hidden">
-            <DarkModeButton />
-            <Presentation />
-            <Experiences experiences={experiences} />
-            <CodeExamples projects={projects} />
-            <Contact />
-        </div>
+        <>
+            <Head>
+                <title>Alexandre Mathieu</title>
+                <link rel="icon" href="./favicon.ico" />
+                <meta name="og:title" content="Alexandre Mathieu's Portfolio" />
+                <meta property="og:type" content="website" />
+                <meta property="og:site_name" content="Alexandre Mathieu" />
+                <meta
+                    name="description"
+                    content="This website portrays Alexandre Mathieu's experiences as an aspiring software engineer. Currently completing his software engineering baccalaureate at Université Laval, he aims to diversify his experiences and produce solid, scalable and clean code."
+                ></meta>
+                <meta
+                    name="og:description"
+                    content="This website portrays Alexandre Mathieu's experiences as an aspiring software engineer. Currently completing his software engineering baccalaureate at Université Laval, he aims to diversify his experiences and produce solid, scalable and clean code."
+                ></meta>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+            </Head>
+            <main>
+                <div className="bg-lightMode dark:bg-darkMode overflow-x-hidden overflow-y-hidden">
+                    <DarkModeButton />
+                    <Presentation />
+                    <Experiences experiences={experiences} />
+                    <CodeExamples projects={projects} />
+                    <Contact />
+                </div>
+            </main>
+        </>
     );
 }
